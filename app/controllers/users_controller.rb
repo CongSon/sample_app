bf856abd1cf4fe8f4fc569278cb575264adfc1ef
Nguyen Cong Son <sonncse03032@gmail.com>
2016-11-10 15:14:53 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:show, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def show
     @user = User.find_by id: params[:id]
     unless @user
-      raise ActiveRecord::RecordNotFound.new I18n.t ".not_found"
+      render_404
     end
   end
 
@@ -21,9 +25,57 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by id: params[:id]
+    unless @user
+      render_404
+    end
+  end
+
+  def update
+    @user = User.find_by id: params[:id]
+    if @user.update_attributes user_params
+      flash[:success] = I18n.t ".update_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @users = User.page(params[:page]).order("created_at ASC")
+  end
+
+  def destroy
+    User.find_by(id: params[:id]).destroy
+    flash[:success] = I18n.t ".delete_success"
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
       :password_confirmation)
+  end
+
+  def logged_in_user
+    unless  logged_in?
+      store_location
+      flash[:danger] = I18n.t ".must_login"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html",  status: 404
   end
 end
